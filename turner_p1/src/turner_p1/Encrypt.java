5e8e3c5e36a9ebdbc415ba1e8267519a62b31efe
Owner <Owner@10.0.0.75>
2019-09-03 18:17:17 -0400

    First commit of project just to get things started

---
+++ turner_p1/src/turner_p1/Encrypt.java
@@ -0,0 +1,31 @@
+//Cydni Turner
+//COP 3330
+//Assignment 2 - Problem 1
+
+package turner_p1;
+
+import java.util.Scanner;
+
+public class Encrypt {
+
+	public static void main(String[] args) {
+		
+		//Declare variables 
+		Scanner scnr = new Scanner(System.in);
+		int Num;
+		
+		//Ask user for input
+		System.out.println("* * Encryption Application * *\n");
+		System.out.println("Enter a 4 digit integer: ");
+		
+		//Get integer
+		Num = scnr.nextInt();
+		
+		/*Int var Num will probably need to be changed to an array to 
+		 be able to work with each individual digit buuut I haven't learned that
+		 yet sooo */
+		
+
+	}
+
+}
