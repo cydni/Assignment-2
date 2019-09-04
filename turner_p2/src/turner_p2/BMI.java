747ca88d78b326a9e67234f9eb94b8bddb18df2f
Owner <Owner@10.0.0.75>
2019-09-04 10:02:33 -0400

    if else statements started

--- turner_p2/src/turner_p2/BMI.java
+++ turner_p2/src/turner_p2/BMI.java
@@ -11,14 +11,50 @@ public class BMI {
 	System.out.println("* * BMI Calculator * *");
 		
 	//Declare Variables
-	int height, weight, choice;
+	int height, weight, bmi, choice;
 	Scanner scnr = new Scanner(System.in);
 	
 	//Get an option
-	System.out.print("\nCALCULATION OPTIONS: \n\n1. Pounds and inches \n\n2. Kilos and meters");
+	System.out.println("\nCALCULATION OPTIONS: \n\n1. Pounds and inches \n\n2. Kilos and meters\n");
 	System.out.print("Enter your choice (1 or 2): ");
 	
 	choice = scnr.nextInt();
+	
+	if(choice == 1) {
+		
+	System.out.println("\nYou selected pounds and inches");
+	
+	//Get height
+	System.out.print("\nEnter your height: ");
+	height = scnr.nextInt();
+	
+	//Get weight
+	System.out.print("\nEnter your weight: ");
+	weight = scnr.nextInt();
+	
+	
+		
+	}
+	else if (choice == 2) {
+		
+	System.out.println("\nYou selected kilos and meters");
+	
+	//Get height
+    System.out.print("\nEnter your height: ");
+	height = scnr.nextInt();
+		
+	//Get weight
+	System.out.print("\nEnter your weight: ");
+	weight = scnr.nextInt();
+		
+	}
+	else {
+		
+	//User has not chosen a valid option
+	System.out.println("\nINVALID CHOICE");
+	
+	}
+
 
 	}
 
