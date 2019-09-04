95da6b5cc5e59992a525ddb985ea5f716bcd5b78
Owner <Owner@10.0.0.75>
2019-09-03 21:25:26 -0400

    First commit w/ Decrypt.java

--- turner_p1/src/turner_p1/Decrypt.java
+++ turner_p1/src/turner_p1/Decrypt.java
@@ -1,9 +1,43 @@
+//Cydni Turner
+
 package turner_p1;
+
+import java.util.Scanner;
 
 public class Decrypt {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Declare variables 
+		Scanner scnr = new Scanner(System.in);
+		int n, n1, n2, n3, n4;
+		int swap;
+		
+		//Ask user for input
+		System.out.println("* * Decryption Application * *\n");
+		System.out.print("Enter a 4 digit code: ");
+		
+		//Get code
+		n = scnr.nextInt();
+		
+		//Code to split an integer into its individual digits
+		n1 = n % 10;
+		n = n / 10;
+		n2 = n % 10;
+		n = n / 10;
+		n3 = n % 10;
+		n = n / 10;
+		n4 = n % 10;
+		n = n / 10;
+		
+		//Test
+		
+		System.out.printf("\nCode Entered: %d%d%d%d", n4, n3, n2,n1);
+				
+		
+		
+		
+		
 
 	}
 
