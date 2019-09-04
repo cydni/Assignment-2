cb5969fd6c47e7af87f88ff2d055d49b23de1e99
Owner <Owner@10.0.0.75>
2019-09-03 21:47:44 -0400

    Decrypt.java complete

--- turner_p1/src/turner_p1/Decrypt.java
+++ turner_p1/src/turner_p1/Decrypt.java
@@ -30,13 +30,28 @@ public class Decrypt {
 		n4 = n % 10;
 		n = n / 10;
 		
-		//Test
 		
 		System.out.printf("\nCode Entered: %d%d%d%d", n4, n3, n2,n1);
+		
+		//Carry out swapping first and third digits (n4 and n2)
+		swap = n4;
+		n4 = n2;
+		n2 = swap;
 				
+		//Carry out swapping second and fourth digits (n3 and n1)
+		swap = n3;
+		n3 = n1;
+		n1 = swap;
 		
+		//Carry out Calculations
+		n4 = (n4+10)-7;
+		n3 = (n3+10)-7;
+		n2 = (n2+10)-7;
+		n1 = (n1+10)-7;
 		
-		
+		//Print Decrypted Code
+		System.out.printf("\n\nDecrypted Code: %d%d%d%d", n4, n3, n2,n1);
+				
 		
 
 	}
