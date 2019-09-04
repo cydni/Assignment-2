35bfe1748e29180df06c203c10d3127da9d5bb30
Owner <Owner@10.0.0.75>
2019-09-03 22:10:28 -0400

    Commit #2

--- turner_p2/src/turner_p2/BMI.java
+++ turner_p2/src/turner_p2/BMI.java
@@ -1,9 +1,24 @@
+//Cydni Turner
+
 package turner_p2;
+
+import java.util.Scanner;
 
 public class BMI {
 
 	public static void main(String[] args) {
 		
+	System.out.println("* * BMI Calculator * *");
+		
+	//Declare Variables
+	int height, weight, choice;
+	Scanner scnr = new Scanner(System.in);
+	
+	//Get an option
+	System.out.print("\nCALCULATION OPTIONS: \n\n1. Pounds and inches \n\n2. Kilos and meters");
+	System.out.print("Enter your choice (1 or 2): ");
+	
+	choice = scnr.nextInt();
 
 	}
 
