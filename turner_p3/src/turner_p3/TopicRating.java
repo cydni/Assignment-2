50cac5c32156dd12582b690b4006141eca318751
cydni <cydni@10.40.104.124>
2019-09-20 17:50:15 -0400

    Topics initialized in array

--- turner_p3/src/turner_p3/TopicRating.java
+++ turner_p3/src/turner_p3/TopicRating.java
@@ -13,6 +13,18 @@ public class TopicRating {
 		String[] topic = new String[5];
 		int [][] responses = new int [5][10];
 		
+		topic[0] = "political issues";
+		topic[1] = "environmental issues";
+		topic[2] = "education";
+		topic[3] = "sports";
+		topic[4] = "religion";
+		
+		for(int i = 0; i<5; ++i) {
+			
+			System.out.println(topic[i]);
+			
+		}
+		
 	}
 
 }
