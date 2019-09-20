9172d3f6a866a8d5e264af152d39c4a7c0e800b1
cydni <cydni@10.40.104.124>
2019-09-20 18:25:30 -0400

    Started working on nested for loops

--- turner_p3/src/turner_p3/TopicRating.java
+++ turner_p3/src/turner_p3/TopicRating.java
@@ -10,18 +10,29 @@ public class TopicRating {
 		
 		//Declare all needed items
 		Scanner scnr = new Scanner(System.in);
+		
 		String[] topic = new String[5];
 		int [][] responses = new int [5][10];
 		
-		topic[0] = "political issues";
-		topic[1] = "environmental issues";
-		topic[2] = "education";
-		topic[3] = "sports";
-		topic[4] = "religion";
+		int i; //row counter
+		int j; //column counter
 		
-		for(int i = 0; i<5; ++i) {
+		topic[0] = "Politics";
+		topic[1] = "Environment";
+		topic[2] = "Education";
+		topic[3] = "Sports";
+		topic[4] = "Religion";
+		
+		for(j = 0; j<10; ++j) { //column counting loop
 			
-			System.out.println(topic[i]);
+			System.out.println("Hello poller number " + (j+1) + ". Rate the importance of each topic on a scale of 1 to 10. ");
+			
+			for(i = 0; i<5; ++i) { //row counting loop
+				
+		
+				
+				
+			}
 			
 		}
 		
