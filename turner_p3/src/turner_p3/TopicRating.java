97cf23752f988c2da3cd19d5935d8eabbb173027
Owner <Owner@10.40.96.37>
2019-09-19 23:54:01 -0400

    Variable Declarations

--- turner_p3/src/turner_p3/TopicRating.java
+++ turner_p3/src/turner_p3/TopicRating.java
@@ -1,10 +1,18 @@
+//Cydni Turner
+
 package turner_p3;
+
+import java.util.Scanner;
 
 public class TopicRating {
 
 	public static void main(String[] args) {
 		
-
+		//Declare all needed items
+		Scanner scnr = new Scanner(System.in);
+		String[] topic = new String[5];
+		int [][] responses = new int [5][10];
+		
 	}
 
 }
