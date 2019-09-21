b8aeb06b19cee5eb8244d65843c257f26552dfe3
cydni <cydni@10.40.104.124>
2019-09-21 01:44:59 -0400

    tabular data complete

--- turner_p3/src/turner_p3/TopicRating.java
+++ turner_p3/src/turner_p3/TopicRating.java
@@ -19,7 +19,6 @@ public class TopicRating {
 		int j; //column counter
 		char rate = 'Y'; //variable to let new poller add their responses
 		int pollerNum = 1;
-		//FIX ME: DO I REALLY NEED THIS VARIABLE?
 		int rating; //variable to hold ratings from 1-10 
 		
 		//initialize response array to 0 for all cells
@@ -74,7 +73,6 @@ public class TopicRating {
 				avgs[i] = avgs[i] + (responses[i][j] * (j+1));
 			}
 			avgs[i] = avgs[i]/(responses[i][0] + responses[i][1] + responses[i][2] + responses[i][3] + responses[i][4] + responses[i][5] + responses[i][6] + responses[i][7] + responses[i][8] + responses[i][9]);
-			//System.out.println("Average of" + topic[i] + " is: " + avgs[i]);
 		}
 		
 		//Formatting
@@ -82,18 +80,18 @@ public class TopicRating {
 		
 		//Print data in table form 
 		
-		System.out.println("TOPICS       | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 |");
-		System.out.println("-------------|---|---|---|---|---|---|---|---|---|----|");
-		System.out.println(topic[0] + "     | " + responses[0][0] + " | " + responses[0][1] + " | " + responses[0][2] + " | " + responses [0][3] + " | " + responses[0][4] + " | " + responses[0][5] + " | " + responses[0][6] + " | " + responses[0][7] + " | " + responses[0][8] + " | " + responses[0][9] + "  | ");
-		System.out.println("-------------|---|---|---|---|---|---|---|---|---|----|");
-		System.out.println(topic[1] + "  | " + responses[1][0] + " | " + responses[1][1] + " | " + responses[1][2] + " | " + responses [1][3] + " | " + responses[1][4] + " | " + responses[1][5] + " | " + responses[1][6] + " | " + responses[1][7] + " | " + responses[1][8] + " | " + responses[1][9] + "  | ");
-		System.out.println("-------------|---|---|---|---|---|---|---|---|---|----|");
-		System.out.println(topic[2] + "    | " + responses[2][0] + " | " + responses[2][1] + " | " + responses[2][2] + " | " + responses [2][3] + " | " + responses[2][4] + " | " + responses[2][5] + " | " + responses[2][6] + " | " + responses[2][7] + " | " + responses[2][8] + " | " + responses[2][9] + "  | ");
-		System.out.println("-------------|---|---|---|---|---|---|---|---|---|----|");
-		System.out.println(topic[3] + "       | " + responses[3][0] + " | " + responses[3][1] + " | " + responses[3][2] + " | " + responses [3][3] + " | " + responses[3][4] + " | " + responses[3][5] + " | " + responses[3][6] + " | " + responses[3][7] + " | " + responses[3][8] + " | " + responses[3][9] + "  | ");
-		System.out.println("-------------|---|---|---|---|---|---|---|---|---|----|");
-		System.out.println(topic[4] + "     | " + responses[4][0] + " | " + responses[4][1] + " | " + responses[4][2] + " | " + responses [4][3] + " | " + responses[4][4] + " | " + responses[4][5] + " | " + responses[4][6] + " | " + responses[4][7] + " | " + responses[4][8] + " | " + responses[4][9] + "  | ");
-		System.out.println("------------------------------------------------------");
+		System.out.println("TOPICS       | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | AVERAGES");
+		System.out.println("-------------|---|---|---|---|---|---|---|---|---|----|---------");
+		System.out.printf(topic[0] + "     | " + responses[0][0] + " | " + responses[0][1] + " | " + responses[0][2] + " | " + responses [0][3] + " | " + responses[0][4] + " | " + responses[0][5] + " | " + responses[0][6] + " | " + responses[0][7] + " | " + responses[0][8] + " | " + responses[0][9] + "  |  %.2f", avgs[0]);
+		System.out.println("\n-------------|---|---|---|---|---|---|---|---|---|----|---------");
+		System.out.printf(topic[1] + "  | " + responses[1][0] + " | " + responses[1][1] + " | " + responses[1][2] + " | " + responses [1][3] + " | " + responses[1][4] + " | " + responses[1][5] + " | " + responses[1][6] + " | " + responses[1][7] + " | " + responses[1][8] + " | " + responses[1][9] + "  |  %.2f" , avgs[1]);
+		System.out.println("\n-------------|---|---|---|---|---|---|---|---|---|----|---------");
+		System.out.printf(topic[2] + "    | " + responses[2][0] + " | " + responses[2][1] + " | " + responses[2][2] + " | " + responses [2][3] + " | " + responses[2][4] + " | " + responses[2][5] + " | " + responses[2][6] + " | " + responses[2][7] + " | " + responses[2][8] + " | " + responses[2][9] + "  |  %.2f" , avgs[2]);
+		System.out.println("\n-------------|---|---|---|---|---|---|---|---|---|----|---------");
+		System.out.printf(topic[3] + "       | " + responses[3][0] + " | " + responses[3][1] + " | " + responses[3][2] + " | " + responses [3][3] + " | " + responses[3][4] + " | " + responses[3][5] + " | " + responses[3][6] + " | " + responses[3][7] + " | " + responses[3][8] + " | " + responses[3][9] + "  |  %.2f" , avgs[3]);
+		System.out.println("\n-------------|---|---|---|---|---|---|---|---|---|----|---------");
+		System.out.printf(topic[4] + "     | " + responses[4][0] + " | " + responses[4][1] + " | " + responses[4][2] + " | " + responses [4][3] + " | " + responses[4][4] + " | " + responses[4][5] + " | " + responses[4][6] + " | " + responses[4][7] + " | " + responses[4][8] + " | " + responses[4][9] + "  |  %.2f" , avgs[4]);
+		System.out.println("\n----------------------------------------------------------------");
 	}
 
 }
